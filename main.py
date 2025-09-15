490635280e2b1163eeb76f74a53eb2b8109fd8a7
JohnAstro <jonathan956g@gmail.com>
2025-09-14 20:13:16 -0500

    add circleshape and player classes

--- main.py
+++ main.py
@@ -7,13 +7,10 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
-
         # Process player inputs
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -21,8 +18,9 @@ def main():
                 raise SystemExit
         
         screen.fill("black") # Fill the display with a solid color
-
         pygame.display.flip() # Refresh on-screen display 
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
