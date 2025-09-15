5e5eb46eac4012cc1e3993e2e6383909fdb36792
JohnAstro <jonathan956g@gmail.com>
2025-09-14 20:21:22 -0500

    adds player in main and draw it on screen

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         # Process player inputs
@@ -18,7 +18,10 @@ def main():
                 raise SystemExit
         
         screen.fill("black") # Fill the display with a solid color
-        pygame.display.flip() # Refresh on-screen display 
+        player.draw(screen) # draw player
+        pygame.display.flip() # Refresh on-screen display
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
